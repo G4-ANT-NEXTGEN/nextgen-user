de3e92086281656ea696432dac5fddfadb77979e
damrey <chendomrey@gmail.com>
2026-01-23 12:43:17 +0700

    chat page

--- src/stores/chat.js
+++ src/stores/chat.js
@@ -1,19 +1,73 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 import api from '@/api/api';
 export const useChatStore = defineStore('chat', () => {
-    const chats = ref();
-    const fetchChats = async()=>{
-        try{
+  const chats = ref([]);
+  const receivedChats=ref([]);
+  const fetchChats = async () => {
+    try {
 
-            const res= await api.get(`/api/profile/sent-messages`)
-            chats.value=res.data.data;
-        }
-        catch(e){
-            console.log("Error in fetching chat list",e);
-        }
+      const res = await api.get(`/api/profile/sent-messages`)
+      chats.value = res.data.data;
     }
-    return{chats,
-        fetchChats
+    catch (e) {
+      console.log("Error in fetching chat list", e);
     }
-});
+  }
+  const fetchReceivedChats = async () => {
+    try{
+      const res=await api.get(`/api/profile/received-messages`);
+      receivedChats.value=res.data.data;
+    }
+    catch(e){
+      console.log("Error in fetching received chat list", e);
+    }
+  }
+  //get messages by chatId
+  const getOwnMessages = (chatId) => {
+    return chats.value
+      .filter(chat => chat.receiver.id === chatId).sort(
+        (a, b) => new Date(a.created_at) - new Date(b.created_at)
+      )
+
+  }
+   const getReceivedMessages = (chatId) => {
+    return receivedChats.value
+      .filter(chat => chat.sender.id === chatId).sort(
+        (a, b) => new Date(a.created_at) - new Date(b.created_at)
+      )
+
+  }
+
+  const chatList = computed(() => {
+    const map = new Map()
+
+    chats.value.forEach(chat => {
+      if (!chat.receiver) return   // ✅ safety
+
+      const rid = chat.receiver.id
+
+      if (!map.has(rid)) {
+        map.set(rid, {
+          receiver: chat.receiver,
+          messages: []
+      })
+      }
+
+      map.get(rid).messages.push(chat)
+    })
+
+
+
+    return Array.from(map.values())
+  })
+
+  return {
+    chats,
+    fetchChats,
+    getOwnMessages,
+    getReceivedMessages,
+    fetchReceivedChats,
+    chatList
+  }
+})
--- src/views/chat/ChatListView.vue
+++ src/views/chat/ChatListView.vue
@@ -2,65 +2,108 @@
   <div class="chat-section container-fluid">
     <div class="row">
       <div class="col-3">
-        <div class="p-3 chat-list d-flex justify-content-start align-items-start">
-          <div>
-            <div class="chat" v-for="chat in chatStore.chats" :key="chat.id">
-              <base-button class="text-start" type="button" variant="white">
-                <div class="d-flex align-items-center">
-                  <img
-                    :src="chat.receiver.avatar"
-                    class="rounded-5 me-2"
-                    width="60"
-                    height="60"
-                    alt=""
-                  />
-                  <div>
-                    <p class="mb-0 fw-bold">{{ chat.receiver.full_name }}</p>
-                    <span class="message">You : {{ chat.message }}</span>
-                  </div>
-                </div></base-button
-              >
-            </div>
+        <div class="chat-list">
+          <div class="chat" v-for="chat in chatStore.chatList" :key="chat.id">
+            <base-button class="text-start" type="button" variant="white" @click="selectChat(chat.receiver.id)">
+              <div class="d-flex align-items-center">
+                <img :src="chat.receiver.avatar" class="rounded-5 me-2" width="60" height="60" alt="">
+                <div>
+                  <p class="mb-0 fw-bold">{{ chat.receiver.full_name }}</p>
+                  <span class="message">You : <b>{{ chat.messages[0].message }}</b></span>
+                </div>
+              </div>
+            </base-button>
           </div>
+
         </div>
       </div>
-      <div class="col-9 bg-primary rounded-3 ">
-        <div class="main-content d-flex justify-content-center align-items-center">
+
+      <div class="col-9">
+        <div v-if="isSelectChat" class="main-content">
+          <h1>{{ chatStore.getOwnMessages(receiverId)[0].receiver.full_name }}</h1>
+          <div>
+            <div>receiver from </div>
+            <div v-for="receive in chatStore.getReceivedMessages(receiverId)" :key="receive.id">
+              <p>{{ receive.message }}</p>
+            </div>
+            <div class="text-end">
+              <div v-for="message in chatStore.getOwnMessages(receiverId)" :key="message.id">
+                <!-- <p>Receiver : {{ message.receiver.full_name }}</p> -->
+                <p>{{ message.message }}</p>
+                <!-- <p v-for="m in message.messages" :key="m.id">{{ m.message }}</p> -->
+              </div>
+            </div>
+            <input type="text" class="form-control">
+            <base-input/
+          </div>
+
+        </div>
+        <div v-else class="main-content d-flex justify-content-center align-items-center">
           <h3>Start message now...</h3>
         </div>
       </div>
     </div>
   </div>
 
-  <!--  -->
-  <!-- <p>{{ chatStore.chats }}</p> -->
 </template>
 <script setup>
 import { onMounted } from 'vue'
 import { useChatStore } from '@/stores/chat'
+import { ref } from 'vue'
+
+
 
 const chatStore = useChatStore()
+const isSelectChat = ref(false)
+const receiverId = ref(null)
+const senderId = ref(null)
+const selectChat = (id) => {
+  senderId.value = id;
+  isSelectChat.value = true;
+  receiverId.value = id;
+  chatStore.getOwnMessages(id);
+  console.log('spacific receiver : ', chatStore.getOwnMessages(id));
+
+  // console.log('the value in chat list: ',chatStore.chatList)
+}
 onMounted(() => {
   chatStore.fetchChats()
-  console.log(chatStore.chats)
+  chatStore.fetchReceivedChats()
+  // chatStore.chatList
 })
 </script>
 <style scoped>
 .main-content {
   /* max-height: 100vh; */
-  height: 100%;
-}
-.chat-section {
-  background: #f5f1f1;
-  height: calc(100vh - 32px);
-  padding: 16px ;
-}
-.chat-list {
-  background: rgb(255, 255, 255);
-  height: calc(100vh - 64px);
-  /* margin: 16px 0; */
+  background: #f5f5f5;
   border-radius: 10px;
+  height: 100%;
+  padding: 10px;
 }
+
+.chat-section {
+  background: white;
+  height: 100vh;
+  padding: 16px;
+}
+
+.chat-list {
+  background: #f5f5f5;
+  height: calc(100vh - 32px);
+  border-radius: 10px;
+  width: 100%;
+}
+
+.chat {
+  transition: all 0.5s;
+  width: 100%;
+}
+
+.chat:hover {
+  background: white;
+
+}
+
 .message {
   color: gray;
   font-size: 14px;
