a17c22404de43f4cdf32a932beaf8118aab411e0
damrey <chendomrey@gmail.com>
2026-01-25 07:43:17 +0700

    separet chat list and chat room

--- src/views/chat/ChatListView.vue
+++ src/views/chat/ChatListView.vue
@@ -1,54 +1,25 @@
 <template>
   <div class="chat-section container-fluid">
     <div class="row">
-      <div class="col-3">
+      <div class="">
         <div class="chat-list">
+
           <div class="chat" v-for="chat in chatStore.chatList" :key="chat.id">
-            <base-button class="text-start" type="button" variant="white" @click="selectChat(chat.messages[0].isMine ? chat.receiver.id : chat.sender.id)">
-              <div class="d-flex align-items-center">
-                <img :src="chat.messages[0].isMine ? chat.receiver.avatar : chat.sender.avatar" class="rounded-5 me-2" width="60" height="60" alt="">
-                <div>
-                  <p class="mb-0 fw-bold">{{chat.messages[0].isMine ? chat.receiver.full_name : chat.sender.full_name }}</p>
-                  <div class="message">{{chat.messages[0].isMine ? 'You : ': `${chat.sender.full_name} : ` }}{{ chat.messages[0].message }}</div>
+            <router-link class="text-start conversationer text-decoration-none"  :to="{ name: 'chat-room', params: { id: chat.messages[0].isMine ? chat.receiver.id : chat.sender.id }}" type="button" variant="white"
+            >
+                <div class="d-flex align-items-center">
+                  <img :src="chat.messages[0].isMine ? chat.receiver.avatar : chat.sender.avatar" class="rounded-5 me-2"
+                    width="60" height="60" alt="">
+                  <div>
+                    <p class="mb-0 fw-bold">{{ chat.messages[0].isMine ? chat.receiver.full_name : chat.sender.full_name }}
+                    </p>
+                    <div class="message">{{ chat.messages[0].isMine ? 'You : ' : `${chat.sender.full_name} : ` }}{{
+                      chat.messages[0].message }}</div>
+                  </div>
                 </div>
-              </div>
-            </base-button>
-          </div>
+              </router-link >
 
-        </div>
-      </div>
-
-      <div class="col-9">
-        <div v-if="isSelectChat" class="main-content">
-          <div class="receiver-name">
-            <div class="d-flex align-items-center h-100 px-3">
-              <img class="rounded-5 me-2" :src="chatStore.getConversationMessages(receiverId)[0].isMine ? chatStore.getConversationMessages(receiverId)[0].receiver.avatar : chatStore.getConversationMessages(receiverId)[0].sender.avatar" alt="" width="60" height="60">
-              <h4 class="m-0">{{ chatStore.getConversationMessages(receiverId)[0].isMine ? chatStore.getConversationMessages(receiverId)[0].receiver.full_name : chatStore.getConversationMessages(receiverId)[0].sender.full_name }}</h4>
             </div>
-          </div>
-          <div class="px-3 pt-5">
-            <div v-for="receive in chatStore.getConversationMessages(receiverId)" :key="receive.id">
-              <div v-if="!receive.isMine">
-
-                <img class="bg-danger rounded-5 me-2" :src="chatStore.getConversationMessages(receiverId)[0].isMine ? chatStore.getConversationMessages(receiverId)[0].receiver.avatar : chatStore.getConversationMessages(receiverId)[0].sender.avatar"" alt="" width="30" height="30">
-                <p class="receive-message">{{ receive.message }}</p>
-              </div>
-              <div class="text-end" v-else>
-                <p class="send-message">{{ receive.message }}</p>
-              </div>
-            </div>
-            <div class="input-message d-flex">
-
-              <base-input class="w-100" v-model="message" placeholder="send message..."></base-input>
-              <base-button class="btn-send-message" @click="sendMessage"><i class="bi bi-send"></i></base-button>
-            </div>
-            <!-- <input type="text" class="form-control"> -->
-
-          </div>
-
-        </div>
-        <div v-else class="main-content d-flex justify-content-center align-items-center">
-          <h3>Start message now...</h3>
         </div>
       </div>
     </div>
@@ -58,52 +29,14 @@
 <script setup>
 import { onMounted } from 'vue'
 import { useChatStore } from '@/stores/chat'
-import { ref } from 'vue'
 
-
-const message = ref('')
 const chatStore = useChatStore()
-const isSelectChat = ref(false)
-const receiverId = ref(null)
-const senderId = ref(null)
-const selectChat = (id) => {
-  senderId.value = id;
-  isSelectChat.value = true;
-  receiverId.value = id;
-  chatStore.getConversationMessages(id);
-  console.log('spacific receiver : ', chatStore.getConversationMessages(id));
-
-  // console.log('the value in chat list: ',chatStore.chatList)
-}
-const sendMessage = async () => {
-  const formData = new FormData();
-  formData.append('message', message.value);
-  formData.append('receiver_id', receiverId.value);
-  await chatStore.sendMessage(formData);
-  message.value = '';
-
-}
-onMounted(async() => {
+onMounted(async () => {
   await chatStore.fetchChats()
-  await chatStore.fetchReceivedChats()
-  // chatStore.chatList
-  console.log('all conversation : ',chatStore.chatList)
-  // console.log('your own conversation : ',chatStore.chats)
-  // console.log('you received conversation : ',chatStore.receivedChats)
+  console.log('all conversation : ', chatStore.chatList)
 })
 </script>
 <style scoped>
-.main-content {
-  /* max-height: 100vh; */
-  /* width: 80%; */
-  background: #f5f5f5;
-  border-radius: 10px;
-  height: calc(100vh - 108px);
-  padding-bottom: 36px;
-  /* margin-bottom: 46px; */
-  overflow-y: scroll;
-}
-
 .chat-section {
   background: #f5f5f5;
   height: 100vh;
@@ -116,86 +49,35 @@ onMounted(async() => {
   border-radius: 10px;
   width: 100%;
 }
-
 .chat {
   transition: all 0.5s;
   width: 100%;
 }
-
 .chat:hover {
-
   background: #f5f5f5;
-
 }
-
-.receiver-name {
-  position: sticky;
-  background: white;
-  border-radius: 16px 16px 0px 0px;
+.conversationer {
+  align-content: center;
+  padding: 10px;
   height: 80px;
-  margin-top:-36px;
-  top: 0;
+  width: 100%;
+  color:black
 
 }
 
-.input-message {
-  position: fixed;
-  align-self: center;
-  padding-top: 16px;
-  padding-inline: 16px;
-  border-radius: 0px 0px 16px 16px;
-  bottom: 16px;
-  width: 72%;
-  margin-left: -16px;
-  background: white;
-}
-.receive-message {
-  max-width: 60%;
-  height: auto;
-  word-wrap: break-word;
-  overflow-wrap: break-word;
-  background: white;
-  /* color: white; */
-  padding: 8px 12px;
-  border-radius: 10px;
-  display: inline-block;
-  margin-bottom: 5px;
-}
-
-.send-message {
-  max-width: 60%;
-  height: auto;
-  word-wrap: break-word;
-  overflow-wrap: break-word;
-  background: #131313;
-  color: white;
-  padding: 8px 12px;
-  border-radius: 10px;
-  display: inline-block;
-  margin-bottom: 5px;
+.conversationer.active {
+  background-color: #f5f5f5;
+  /* border-left: 4px solid #4f46e5; */
+  font-weight: 600;
 }
 
 .message {
-  /* background: red; */
   max-width: 250px;
-
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
   height: auto;
-  /* overflow-x: scroll; */
   color: gray;
   font-size: 14px;
-}
-
-.btn-send-message {
-  margin-left: 8px;
-  /* color:red; */
-  font-size: 24px;
-  /* margin-top:-14px; */
-  width: 8%;
-  height: 60px;
-  padding: 10px;
-  /* align-self: center; */
 }
 </style>
